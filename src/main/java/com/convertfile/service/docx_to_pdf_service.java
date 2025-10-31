d75c4b112a62d3717034c8301c313720c73370a0
capkimkhanh2k5 <capkimkhanh2k5@gmail.com>
2025-10-31 10:54:51 +0700

    Test convert docx to pdf

---
+++ src/main/java/com/convertfile/service/docx_to_pdf_service.java
@@ -0,0 +1,45 @@
+package com.convertfile.service;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+import java.io.OutputStream;
+
+
+import org.docx4j.*;
+import org.docx4j.Docx4J;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.fonts.IdentityPlusMapper;
+import org.docx4j.fonts.Mapper;
+import org.docx4j.fonts.PhysicalFonts;
+
+//Convert file from docx to pdf
+public class docx_to_pdf_service {
+    public void convertDocxtoPdf (String docxPath, String pdfPath) throws IOException{
+        System.out.println("Start convert docx to pdf: " + docxPath);
+        try {
+            OutputStream output = new FileOutputStream(new File(pdfPath));
+
+            WordprocessingMLPackage wordPackage = WordprocessingMLPackage.load(new File(docxPath));
+            
+            //Font Set
+            Mapper font = new IdentityPlusMapper();
+            PhysicalFonts.discoverPhysicalFonts();
+
+            wordPackage.setFontMapper(font);
+
+            Docx4J.toPDF(wordPackage, output);
+
+            System.out.println("End convert docx to pdf: " + pdfPath);
+        } catch(Docx4JException edocx){
+            System.out.println("Error: " + edocx.getMessage());
+            edocx.printStackTrace();
+            throw new IOException("Error in convert docx to pdf!");
+        } catch (Exception ex) {
+            System.out.println("Error in process!");
+            ex.printStackTrace();
+            throw new IOException("Not found in Process!");
+        }
+    }
+}
