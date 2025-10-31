5144469fa236fd23f4534ac182a0ad54bc9f7764
capkimkhanh2k5 <capkimkhanh2k5@gmail.com>
2025-10-31 20:36:12 +0700

    update some convert type with docx

--- src/main/java/com/convertfile/test/test_convert.java
+++ src/main/java/com/convertfile/test/test_convert.java
@@ -3,12 +3,12 @@ package com.convertfile.test;
 import java.io.File;
 import java.io.IOException;
 
-import com.convertfile.service.docx_to_pdf_service;
+import com.convertfile.service.microService.docx_to_pdf_service;
 
-public class docx_to_pdf {
+public class test_convert {
     public static void main(String[] args) throws IOException{
-        final String docxPath = "/Users/capkimkhanh/Documents/PC/Nam3.HK1/LTM/CONVERT_FILE/src/main/resources/a.docx";
-        final String pdfPath = "/Users/capkimkhanh/Documents/PC/Nam3.HK1/LTM/CONVERT_FILE/src/main/resources/a.pdf";
+        final String docxPath = "";
+        final String pdfPath = "";
 
         try{
             docx_to_pdf_service convert = new docx_to_pdf_service();
